d5dd26d509d709a3ed0b816e9064fab8c91e3271
Mateusz Baczewski <122082899+baczewskimateusz@users.noreply.github.com>
2024-06-04 20:16:55 +0200

    chenges

--- scripts/script.js
+++ scripts/script.js
@@ -14,14 +14,30 @@ document.addEventListener("DOMContentLoaded", function() {
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
     }
-        });
+    });
+
 
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
+
+        const items = document.querySelectorAll('.items-name-footer');
+        console.log(items)
+                items.forEach(item => {
+                    item.addEventListener('click', function() {
+                        const itemsFooter = this.nextElementSibling;
+                        if (itemsFooter.style.display === "none" || itemsFooter.style.display === "") {
+                            itemsFooter.style.display = "flex";
+                            this.classList.add('active');
+                        } else {
+                            itemsFooter.style.display = "none";
+                            this.classList.remove('active');
+                        }
+                    });
+                });
         });
-        
+
 });
 
 
